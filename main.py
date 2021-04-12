db550c09e4d37717baefb96c9c838f3c5f15ec46
Joao Marcelo Alves <jmarsdalves@gmail.com>
2021-04-12 20:20:18 -0300

    First addition

---
+++ main.py
@@ -0,0 +1,18 @@
+# num = input("Digite um número:")
+# x = num.split(" ")
+# if (len(x)==1):
+#     print("Adiocionar vértices na lista")
+# if (len(x)==2):
+#     print("Adiocionar arestas lista")
+# if (len(x)==3):
+#     print("Adiocionar nomes das arestas lista")
+
+def printMenu():
+    print("=" * 24, " MENU ", "=" * 24)
+
+    print("Deseja criar um grafo direcionado ou nao direcionado")
+
+    print("=" * 56)
+
+
+printMenu()
